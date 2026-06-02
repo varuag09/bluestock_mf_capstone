b8c0403f565d286b92ff0dcbb88d58f2fc59c467
varuag09 <gauravkumar.gk0906@gmail.com>
2026-06-02 07:57:30 +0530

    Initial project structure

---
+++ scripts/live_nav_fetch.py
@@ -0,0 +1,28 @@
+import requests
+import pandas as pd
+
+schemes = {
+    "SBI_Bluechip": 119551,
+    "ICICI_Bluechip": 120503,
+    "Nippon_Large_Cap": 118632,
+    "Axis_Bluechip": 119092,
+    "Kotak_Bluechip": 120841
+}
+
+for name, code in schemes.items():
+
+    url = f"https://api.mfapi.in/mf/{code}"
+
+    response = requests.get(url)
+
+    data = response.json()
+
+    nav_df = pd.DataFrame(data["data"])
+
+    nav_df.to_csv(
+        f"../data/raw/{name}_NAV.csv",
+        index=False
+    )
+
+    print(f"{name} saved successfully")
+    
